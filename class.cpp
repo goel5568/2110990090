2af7162e7cdfc1a7623e5e1aa51aa0f9990a50b1
goel5568 <aditya0090.be21@chitkara.edu.in>
2022-04-12 07:00:38 +0530

    first commit | g8 demo session

---
+++ class.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	int n=1;
+	int sum=1;
+	if (b==0){
+		cout<<1;
+	}
+	else{
+		while(n<=b){
+			int ans=sum*a;
+			sum=ans;
+		n=n+1;
+		}
+		cout<<sum;
+	}
+
+}
